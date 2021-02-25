cf72319b3d85941d698b24533315e522480a8afa
root <root@tethys-staging.byu.edu>
2021-02-25 23:57:40 +0000

    Warehouse_Commit

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_namespace_packages
-from tethys_apps.app_installation import find_resource_files
+from setup_helper import find_resource_files
 import fix_tethys_init_files
 
 # -- Apps Definition -- #
@@ -16,6 +16,7 @@ resource_files += find_resource_files('tethysapp/' + app_package + '/workspaces'
 
 fix_tethys_init_files.fix_tethys_init_files(3)
 
+resource_files += find_resource_files('tethysapp/' + app_package + '/scripts', 'tethysapp/' + app_package)
 setup(
     name=release_package,
     version='1.0',
